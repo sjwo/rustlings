e5c8f76c7177885dc33f9e6ef4e164dc7ab37b84
Stephen Josef Wissow <sjw1000@wildcats.unh.edu>
2020-09-08 22:23:33 -0400

    Worked through Quiz 2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,17 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(amt: i32) -> i32 {
+    let price: i32 = 2;
+    let discount_price: i32 = 1;
+    if amt < 40 {
+        amt * price
+    } else {
+        amt * discount_price
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,18 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
+    let mut s = String::from("hello");
+    let r1 = &mut s;
+    println!("{}", r1);
+    r1.push_str(", for real!");
+    println!("{}", r1);
+    println!("{}", s);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")//.to_string() // ...also works
 }
